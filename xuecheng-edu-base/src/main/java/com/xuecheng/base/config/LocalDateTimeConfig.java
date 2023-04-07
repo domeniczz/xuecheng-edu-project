6c8316e3e2e6864f1165a822d5d59f08327bd072
domeniczz <zhangdomenic@outlook.com>
2023-04-07 20:12:16 +0800

    add course list controller and DTOs + QueryCourseParamsDto + PageParam + PageResult + CourseBaseInfoController add Swagger 2 and Log4J support

---
+++ xuecheng-edu-base/src/main/java/com/xuecheng/base/config/LocalDateTimeConfig.java
@@ -0,0 +1,50 @@
+package com.xuecheng.base.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Domenic
+ * @Classname LocalDateTimeConfig
+ * @Description Set Date Time Format
+ * @Date 4/7/2023 8:07 PM
+ * @Created by Domenic
+ */
+@Configuration
+public class LocalDateTimeConfig {
+
+    /**
+     * 序列化内容
+     * LocalDateTime -> String
+     * 服务端返回给客户端内容
+     */
+    @Bean
+    public LocalDateTimeSerializer localDateTimeSerializer() {
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 反序列化内容
+     * String -> LocalDateTime
+     * 客户端传入服务端数据
+     */
+    @Bean
+    public LocalDateTimeDeserializer localDateTimeDeserializer() {
+        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.serializerByType(LocalDateTime.class, localDateTimeSerializer());
+            builder.deserializerByType(LocalDateTime.class, localDateTimeDeserializer());
+        };
+    }
+
+}
---
+++ xuecheng-edu-base/src/main/java/com/xuecheng/base/model/PageResult.java
@@ -0,0 +1,36 @@
+package com.xuecheng.base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Domenic
+ * @Classname PageResult
+ * @Description 分页查询的响应数据模型类
+ * @Date 4/7/2023 4:16 PM
+ * @Created by Domenic
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PageResult<T> implements Serializable {
+
+    // 数据列表
+    private List<T> items;
+
+    // 总记录数
+    private long counts;
+
+    // 当前页码
+    private long page;
+
+    // 每页记录数
+    private long pageSize;
+
+}
