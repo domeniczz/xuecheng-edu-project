f177170c12e23b4d1ea79a13f1629caaa14803e9
domeniczz <zhangdomenic@outlook.com>
2023-05-17 22:01:45 +0800

    change scheduled task from spring schedlue to xxl-job scheduler; add a sample sharding task

---
+++ xuecheng-edu-media/xuecheng-edu-media-service/src/main/java/com/xuecheng/media/service/jobhandler/MediaProcessXxlJob.java
@@ -0,0 +1,52 @@
+package com.xuecheng.media.service.jobhandler;
+
+import com.xuecheng.media.utils.MinioUtils;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Domenic
+ * @Classname SampleXxlJob
+ * @Description 媒体文件处理任务类
+ * @Created by Domenic
+ */
+@Component
+public class MediaProcessXxlJob {
+
+    @Autowired
+    private MinioUtils minioUtils;
+
+    /**
+     * 存储视频的桶
+     */
+    @Value("${minio.bucket.videofiles}")
+    private String bucket;
+
+    /**
+     * 清理 Minio 中残留分块文件的任务
+     * @throws Exception
+     */
+    @XxlJob("residualFileCleanerJob")
+    public void residualFileCleanerJob() throws Exception {
+        minioUtils.clearResidualChunkFiles(bucket);
+    }
+
+    /**
+     * 示例分片广播任务，将任务发送给所有的执行器
+     * <!-- 本地测试时，启动多个实例 -->
+     * @throws Exception
+     */
+    @XxlJob("sampleSharingJobHandler")
+    public void sampleSharingJobHandler() throws Exception {
+
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+
+        System.out.println("当前分片索引: " + shardIndex + ", 当前分片总数: " + shardTotal);
+    }
+
+}
